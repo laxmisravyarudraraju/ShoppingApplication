ce0d7b2ea548e314ba5e5029ed8d50cc0748a6ad
rlaxmisravya <r.lakshmisravya@gmail.com>
2020-06-12 20:34:14 +0530

    Initialized Category and CategoryList Component.

---
+++ app/components/Category.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Category = (props) => {
+  return <React.Fragment>Hello from Category file</React.Fragment>;
+};
---
+++ app/components/CategoryList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CategoryList = (props) => {
+  return <React.Fragment>Hello from CategoryList file</React.Fragment>;
+};
