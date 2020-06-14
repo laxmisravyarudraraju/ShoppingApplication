16410a64fc94f673577e41fd7ab1da539ebc26a6
rlaxmisravya <r.lakshmisravya@gmail.com>
2020-06-15 00:07:47 +0530

    minor changes...

--- app/components/CategoryList/CategoryList.js
+++ app/components/CategoryList/CategoryList.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 
 import { Category } from "../Category/Category";
 import CategoryListStyles from "./styles";
