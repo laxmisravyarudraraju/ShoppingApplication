bd5a681c37d5ecc365ec2d379fd08961c3ab8a74
rlaxmisravya <r.lakshmisravya@gmail.com>
2020-06-14 11:27:58 +0530

    Refactored the code into separate component folders

--- app/App.js
+++ app/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { Homepage } from "./components/Homepage";
+import { Homepage } from "./components/Homepage/Homepage";
 
 class App extends React.Component {
   render() {
--- app/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = (props) => {
-  return (
-    <React.Fragment>
-      <div> Hello from Footer file </div>
-    </React.Fragment>
-  );
-};
