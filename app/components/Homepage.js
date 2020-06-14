d6ca1b35f5ee0273696c19068c5ea23033e26451
rlaxmisravya <r.lakshmisravya@gmail.com>
2020-06-14 11:17:16 +0530

    changed container width to lg

--- app/components/Homepage.js
+++ app/components/Homepage.js
@@ -6,25 +6,12 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { CategoryList } from "./CategoryList";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
 export const Homepage = (props) => {
-  const classes = useStyles();
-
   return (
-    // <Container maxWidth="lg">
-    <React.Fragment>
+    <Container maxWidth="lg">
       <Header />
       <CategoryList />
       <Footer />
-    </React.Fragment>
-    // </Container>
+    </Container>
   );
 };
